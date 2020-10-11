5c2ffc2667152b66c0233b01b8b0fd5aeb3dc2ab
Henry North <henrynorth@outlook.com>
2020-10-11 14:31:14 +0100

    firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2wk7626g7UEuxueQJHuoZX6em-fOVGCM",
+  authDomain: "better-journal.firebaseapp.com",
+  databaseURL: "https://better-journal.firebaseio.com",
+  projectId: "better-journal",
+  storageBucket: "better-journal.appspot.com",
+  messagingSenderId: "939383302943",
+  appId: "1:939383302943:web:26fbfc4b2699faf1539e2a",
+  measurementId: "G-MC2BX1G14S"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
